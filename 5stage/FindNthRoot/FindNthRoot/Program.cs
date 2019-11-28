f5dda503941bee0576866a123951197aef69680d
Kaznacheev Igor <Kaznacheevii@icloud.com>
2019-11-28 14:45:17 +0400

    1 add 5 stage

---
+++ 5stage/FindNthRoot/FindNthRoot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FindNthRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SqrtN(-0.008, 3, 0.1));
+            Console.ReadKey();
+        }
+
+        //static double Pow(double a, int pow)
+        //{
+        //    double result = 1;
+        //    for (int i = 0; i < pow; i++) result *= a;
+        //    return result;
+        //}
+
+        static double SqrtN(double A, double n, double eps)
+        {
+            int colv = 0;
+            double teps = eps;
+            do
+            {
+                teps *= 10;
+                colv++;
+            } while (teps < 1);
+
+            var x0 = A / n;
+            var x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+            }
+            
+            return Math.Round(x1,colv);
+        }
+    }
+}
---
+++ 5stage/sort/sort/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество строк в массиве");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество чисел в строке");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[][] arr = new int[a][];
+            for (int i = 0; i < a; i++)
+            {
+                arr[i] = new int[b];
+            }
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //BubbleSortSum(arr, ">");
+            BubbleSortMin_Max(arr, ">", "max");
+            PrintArray(arr);
+        }
+
+        private static void PrintArray(int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+        }// вывод матрицы
+
+        static int[][] BubbleSortSum(int[][] mas, string s)
+        {
+            int[] temp;
+            int tmp;
+            int[] zap = new int[mas.Length];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = 0; j < mas[i].Length; j++)
+                {
+                    zap[i] += mas[i][j];
+                }
+            } // счет суммы
+            if (s == ">")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (zap[i] < zap[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                            tmp = zap[i];
+                            zap[i] = zap[j];
+                            zap[j] = tmp;
+                        }
+                    }
+                }
+            } // по убыванию
+            else
+            if (s == "<")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (zap[i] > zap[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                            tmp = zap[i];
+                            zap[i] = zap[j];
+                            zap[j] = tmp;
+                        }
+                    }
+                }
+            } // по возрастанию 
+            
+            return mas;
+        } // сортировка по сумме с параметрами: матрица, знак(убывание>, возрастание<)
+
+        static int[][] BubbleSortMin_Max(int[][] mas, string s, string z)
+        {
+            int[] temp;
+            int tmp;
+            
+            int[] zap = new int[mas.Length];
+            if (z == "max")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = 0; j < mas[i].Length; j++)
+                    {
+                        if (j == 0)
+                        {
+                            zap[i] = mas[i][j];
+                        } // присвоение первого элемента
+                        if (zap[i] < mas[i][j] && j > 0)
+                        {
+                            zap[i] = mas[i][j];
+                        }
+                    }
+                }
+            }// если сортировка по максимальному
+            if (z == "min")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = 0; j < mas[i].Length; j++)
+                    {
+                        if (j == 0)
+                        {
+                            zap[i] = mas[i][j];
+                        } // присвоение первого элемента
+                        if (zap[i] > mas[i][j] && j > 0)
+                        {
+                            zap[i] = mas[i][j];
+                        }
+                    }
+                }
+            }// если сортировка по минимальному
+            if (s == ">")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (zap[i] < zap[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                            tmp = zap[i];
+                            zap[i] = zap[j];
+                            zap[j] = tmp;
+                        }
+                    }
+                }
+            } // по убыванию
+            else
+            if (s == "<")
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (zap[i] > zap[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                            tmp = zap[i];
+                            zap[i] = zap[j];
+                            zap[j] = tmp;
+                        }
+                    }
+                }
+            } // по возрастанию
+
+            return mas;
+        }// сортировка по макс/мин элементу с параметрами: матрица, знак(убывание>, возрастание<), макс/мин(max, min)
+    }
+}
