1f71813c8da56b58ba73aa05d98c389409d11026
Kaznacheev Igor <Kaznacheevii@icloud.com>
2020-01-14 19:23:19 +0400

    add 8 unit

---
+++ 8stage/Stepen/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Stepen
+{
+    public class Mnogochlen
+    {
+        public string mnogochlen = "";
+        public string Vstepen(int stepen)
+        {
+            if (stepen == 0)
+            {
+                mnogochlen = "1";
+            }
+            else if (stepen != 1) 
+            {
+                mnogochlen = "(" + mnogochlen + ")^" + Convert.ToString(stepen);
+            }
+            return mnogochlen;
+        }
+        public static Mnogochlen operator +(Mnogochlen c1, Mnogochlen c2)
+        {
+            if (c1.mnogochlen != c2.mnogochlen) return new Mnogochlen { mnogochlen = c1.mnogochlen + " + " + c2.mnogochlen };
+            else return new Mnogochlen { mnogochlen = "2(" + c1.mnogochlen + ")" };
+        }
+        public static Mnogochlen operator -(Mnogochlen c1, Mnogochlen c2)
+        {
+            if (c1.mnogochlen != c2.mnogochlen) return new Mnogochlen { mnogochlen = c1.mnogochlen + " - " + c2.mnogochlen };
+            else return new Mnogochlen { mnogochlen = "0" };
+        }
+        public static Mnogochlen operator /(Mnogochlen c1, Mnogochlen c2)
+        {
+            if (c1.mnogochlen!= c2.mnogochlen) return new Mnogochlen { mnogochlen = "(" + c1.mnogochlen + ") / (" + c2.mnogochlen + ")" };
+            else return new Mnogochlen { mnogochlen = "1" };
+        }
+        public static Mnogochlen operator *(Mnogochlen c1, Mnogochlen c2)
+        {
+            if (c1.mnogochlen != c2.mnogochlen) return new Mnogochlen { mnogochlen = "(" + c1.mnogochlen + ") * (" + c2.mnogochlen + ")" };
+            else return new Mnogochlen { mnogochlen = "(" + c1.mnogochlen + ")^2" };
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mnogochlen z = new Mnogochlen();
+            Mnogochlen c = new Mnogochlen();
+            c.mnogochlen = "2a + 3c";
+            z.mnogochlen = "2a + 3c";
+            z = z + c;
+            z.Vstepen(5);
+            Console.WriteLine(z.mnogochlen);
+        }
+    }
+}
---
+++ 8stage/geometric_class/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace geometric_class
+{
+    public class Triangle
+    {
+        public double a = 0, b = 0, c = 0;
+        public double getP()
+        {
+            double P = a + b + c;
+            return P;
+        }
+        public double getS()
+        {
+            double p = getP();
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+
+        }
+    }
+    public class Circle
+    {
+        public double Rad = 0;
+        public double getP()
+        {
+            double P = Rad * 2 * Math.PI;
+            return P;
+        }
+        public double getS()
+        {
+            double S = Rad * Rad * Math.PI;
+            return S;
+        }
+    }
+    public class Rectangle
+    {
+        public double lengt = 0;
+        public double hight = 0;
+        public double getP()
+        {
+            double P = lengt * 2 + hight * 2;
+            return P;
+        }
+        public double getS()
+        {
+            double S = lengt * hight;
+            return S;
+        }
+    }
+    public class kvadrat
+    {
+        public double lenght = 0;
+        public double getP()
+        {
+            double P = lenght * 4;
+            return P;
+        }
+        public double getS()
+        {
+            double S = lenght * lenght;
+            return S;
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            kvadrat a = new kvadrat();
+            a.lenght = 1;
+            Console.WriteLine(a.getP());
+        }
+    }
+}
