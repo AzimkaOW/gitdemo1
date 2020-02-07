8db7a980d21badce8ce7936471d836aafa1ad3b5
Igor Kaznacheev <kaznacheevii@icloud.com>
2020-02-07 23:22:11 +0400

    add 11 unit

---
+++ 11unit/11Unit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+namespace _11Unit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Countdown Counter = new Countdown();
+            Alo alo = new Alo();
+
+            Counter.Mes += alo.Message;
+
+            Counter.Count(5);
+
+        }
+        public class Countdown
+        {
+            public delegate void Del();
+            public event Del Mes;
+            public void Count(int j)
+            {
+                
+                for (int i = j; i >= 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(500);
+                }
+                Mes();
+                
+            }
+        }
+        class Alo
+        {
+            public void Message()
+            {
+                Console.WriteLine("it's time to ur message");
+            }
+
+        }
+    }
+}
